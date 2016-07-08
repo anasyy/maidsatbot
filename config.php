53578665e114502becc20df1ecd01a80d1a2e726
Dani Shalash <dani.shalash.manga@gmail.com>
2016-07-08 05:05:18 +0000

    initial commit

--- config.php
+++ config.php
@@ -8,13 +8,13 @@ $verify_token = 'Token123Test';
 //$token='EAAXGfZBD4kzkBANHKInrujrBjZAPvo4zuqLjZADbeZCujhds731tzwDSgS8PmVoa44foDAhYqkii08AJr57NUqIbZCthzST1BsBxiRz2qrecDR2z5YT8a6Ub4pDmeHRZB0oM9trO4qOhyVHlg1SlWxZAV0qEseeDxhIe5CvFsV7GgZDZD';
 $token='EAADZCyJRvqsoBAFPCPacUAdU6o2NXOU9DPJICwtjh6ImZBzYYLMnoJnztqRvHp60E3rYfSA6Pz3YEWoJ5eg2Y5OZBaAkHwMN9alALOXYq2Uctr3VxrJ4A0XBBDkLdVI9oHEVZAegb2nfWsAdlvtZB01LhWDifrUL2hZCaScbEy2QZDZD';
 
-/**
+
 $servername = "127.0.0.1";
 $username = "danimagna";
 $password = "";
 $database = "maidsatbot";
 $dbport = 3306;
-**/
+
 
 
 $sf_username = 'maids.ae@magna.com.sandbox' ; 
@@ -23,12 +23,14 @@ $sf_password = 'RockandRoll#01lKn7hqI3Ykm1j0gg8lfDIE4S' ;
 $sf_wsdl     = dirname(__FILE__).'/salesforce-php-toolkit/soapclient/partner.wsdl.xml';
 
 
+/**
 $dburl ='mysql://adminZvN61vt:BTgWDQ2k_3MA@577e55a97628e16d32000062-mediatestbot.rhcloud.com:51446/';
 $servername = "577e55a97628e16d32000062-mediatestbot.rhcloud.com";
 $username = "adminZvN61vt";
 $password = "BTgWDQ2k_3MA";
 $database = "maidsatbot;
 $dbport = 51446;
+**/
 
 
 function end_flush()
